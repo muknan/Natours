fd2c5ba6daac54f1e899a5532b1ab459f42fdcdc
muknan <muk.msi@gmail.com>
2023-08-07 17:55:36 +0530

    JS for navigation and smooth scrolling

---
+++ js/script.js
@@ -0,0 +1,37 @@
+//Make mobile navigation work
+const btnNavEl = document.querySelector(".navigation__checkbox");
+
+function uncheckBox() {
+  if (btnNavEl.checked) {
+    btnNavEl.checked = false;
+  }
+}
+
+const navEl = document.querySelector(".navigation__list");
+
+navEl.addEventListener("click", function () {
+  uncheckBox();
+});
+
+////////////////////////////////////////////////////////////
+//Implementing smooth scrolling animation
+const allLinks = document.querySelectorAll("a:link");
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    // e.preventDefault();
+    const href = link.getAttribute("href");
+    //Scroll back to top
+    if (href === "#") {
+      e.preventDefault();
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    }
+
+    //Scroll to other links
+    if (href !== "#" && href.startsWith(!"#pop")) {
+      console.log("Test");
+      e.preventDefault();
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+  });
+});
